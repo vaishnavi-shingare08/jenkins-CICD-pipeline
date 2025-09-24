81ae75ee3bdd9ca8ed476d85141fbe14b7d2fb78
EC2 Default User <ec2-user@ip-172-31-10-3.ap-south-1.compute.internal>
2025-09-24 11:31:25 +0000

    Added Flask app, Dockerfile, and Jenkinsfile

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Home page
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# About page
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# Simple form submission
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form['name']
+    return render_template("greet.html", name=name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
